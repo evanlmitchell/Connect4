c7cde15aac03b38b9f67ab1c9534ab3dbedcc3a1
Evan Mitchell <evan@PL-MA-EMitchell.local>
2017-05-16 13:27:03 -0400

    Make methods chainable

--- board.rb
+++ board.rb
@@ -10,16 +10,30 @@ class Board
     @lanes.each_with_index { |lane, position|
       puts "#{position + 1} #{lane}"
     }
+    self
   end
 
-  def add_piece()
+  def add_piece(position, player)
+    #Errors:
+    # Stack is too big
+    # Non-existent position: < 1, > NUMBER_OF_THINGS
+    @lanes[position - 1].push(player)
+    self
   end
 
 end
 
 
+
 board = Board.new
-board.add_piece(4, "B")
-board.add_piece(4, "W")
-board.add_piece(3, "B")
-board.display
+  .add_piece(4, "B")
+  .add_piece(4, "W")
+  .add_piece(3, "B")
+  .add_piece(0, "W")
+  .add_piece(-1, "W")
+  .add_piece(45, "W")
+  .display
+
+board
+  .add_piece(2, "W")
+  .display
