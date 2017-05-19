1757de10f2d4ae38d6c3ecada69529ea5b11f8c1
Evan Mitchell <evan@PL-MA-EMitchell.local>
2017-05-19 13:32:27 -0400

    Prevent invalid moves
    - Set conditional statements within the add_piece function

--- board.rb
+++ board.rb
@@ -1,9 +1,10 @@
 # create and display Connect4 board
 class Board
-  NUMBER_OF_THINGS = 7
+  NUMBER_OF_LANES = 7
+  LANE_CAPACITY = 6
 
   def initialize
-    @lanes = Array.new(NUMBER_OF_THINGS) { [] }
+    @lanes = Array.new(NUMBER_OF_LANES) { [] }
   end
 
   def display
@@ -17,10 +18,17 @@ class Board
     #Errors:
     # Stack is too big
     # Non-existent position: < 1, > NUMBER_OF_THINGS
-    @lanes[position - 1].push(player)
+
+    if (1..NUMBER_OF_LANES).cover?(position)
+      lane = @lanes[position - 1]
+      
+      if lane.length < LANE_CAPACITY
+        lane.push(player)
+      end
+    end
+
     self
   end
-
 end
 
 
@@ -33,6 +41,18 @@ board = Board.new
   .add_piece(-1, "W")
   .add_piece(45, "W")
   .display
+
+puts "---------"
+
+board
+  .add_piece(4, "W")
+  .add_piece(4, "B")
+  .add_piece(4, "W")
+  .add_piece(4, "B")
+  .add_piece(4, "W")
+  .display
+
+puts "---------"
 
 board
   .add_piece(2, "W")
